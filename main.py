90163388099200cb434beb49397288e5c31299c8
Gan Tu <tugan@berkeley.edu>
2017-07-04 03:36:40 -0700

    minor updates for deployment

--- main.py
+++ main.py
@@ -104,5 +104,7 @@ def contact_us():
 
 ##################### Main App #####################
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    app.debug = True
 
