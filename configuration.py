5506e7f9ee9bb61fccf4d387941f6b4931e5ff17
Gan Tu <tugan@berkeley.edu>
2017-07-05 15:18:14 -0700

    move variable declaration for logo urls to configuration files

---
+++ configuration.py
@@ -0,0 +1,25 @@
+
+logo_numbers = 35
+logo_urls = {
+      11: "https://www.space.com/33445-voices-of-humanity-kickstarter-interstellar-flight.html",
+      13: "https://uk.news.yahoo.com/kickstarter-project-aims-back-humanity-cosmic-cloud-183236176.html",
+      15: "https://www.wired.com/2016/07/physicists-want-send-dna-space-lasers/",
+      18: "https://www.cnet.com/news/how-your-tweets-in-space-will-help-spacecraft-approach-light-speed/",
+      21: "https://www.noozhawk.com/article/voices_of_humanity_space_microchip_072716",
+      22: "http://www.popsci.com/humanity-chip-wants-to-send-your-time-capsule-into-space",
+      23: "http://www.dailymail.co.uk/sciencetech/article-3698301/Beam-Radical-project-mankind-sending-laser-propelled-chips-containing-DNA-book-stars.html",
+      24: "http://news.sky.com/story/humanity-could-be-backed-up-on-space-chip-10506839",
+      25: "http://metro.co.uk/2016/07/20/new-plan-will-use-lasers-to-beam-humans-to-other-stars-to-back-up-our-species-6017700/",
+      26: "http://www.express.co.uk/news/science/691407/Human-DNA-SPACE-back-up-species-extinct",
+      27: "http://www.releasewire.com/press-releases/directed-energy-invites-the-public-to-participate-in-its-voices-of-humanity-kickstarter-campaign-money-raised-will-beam-and-launch-your-data-into-space-708292.htm",
+      28: "http://www.digitaljournal.com/pr/3014187",
+      29: "http://macedoniaonline.eu/content/view/29842/56/",
+      30: "http://dailycaller.com/2016/07/18/crowdfunding-campaign-wants-to-send-genetic-information-into-space-using-lasers/",
+      31: "http://www.cbsnews.com/news/kickstarter-project-aims-to-back-up-humanity-in-cosmic-cloud/",
+      32: "https://sputniknews.com/world/201607191043247727-sending-dna-into-space/",
+      33: "http://www.vocativ.com/349677/blast-your-stupid-tweets-to-space/",
+      34: "http://www.spacedaily.com/reports/Directed_Energy_Invites_the_Public_to_Participate_in_its_Voices_of_Humanity_Kickstarter_999.html",
+      35: "https://www.sciencemag.org/news/sifter/you-can-now-send-your-political-frustrations-and-more-stars"
+}
+
+
